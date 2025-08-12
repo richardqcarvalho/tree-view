d433cef7273cf8641b4a15ec35fe2a66bd81671f
Richard Carvalho <richardcarvalhooo@gmail.com>
2025-08-12 18:51:13 -0300

    style: Improving header with logo and some reorganization

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,20 @@
 import Assets from '../components/assets'
 import CompanySelect from '../components/company-select'
 import Locations from '../components/locations'
+import tractianLogo from '../images/logo.svg'
 
 export default function Home() {
   return (
     <div className='flex h-full flex-col'>
-      <div className='bg-header flex flex-row justify-end p-3'>
-        <CompanySelect />
+      <div className='bg-header flex flex-row items-center justify-between p-4'>
+        <img
+          src={tractianLogo}
+          className='h-4'
+        />
+        <div className='flex items-center gap-2'>
+          <span className='text-white'>Unit:</span>
+          <CompanySelect />
+        </div>
       </div>
       <div className='flex flex-col overflow-auto'>
         <Locations />
