e8821a97ad1108051a826ecc465a4609d4aff73b
Richard Carvalho <richardcarvalhooo@gmail.com>
2025-08-14 12:20:16 -0300

    style: Improving component information visualization

--- src/pages/component.tsx
+++ src/pages/component.tsx
@@ -1,6 +1,9 @@
+import EnergySvg from '@/images/energy.svg?react'
+import VibrationSvg from '@/images/vibration.svg?react'
 import { useElementStore } from '@/store'
 import type { StructuredAssetT } from '@/types/asset'
 import type { StructuredLocationT } from '@/types/location'
+import clsx from 'clsx'
 import { useEffect } from 'react'
 import { useNavigate, useParams } from 'react-router'
 
@@ -33,12 +36,41 @@ export default function Component() {
 
   if (!component) return
 
+  const statusClassName = clsx([
+    'w-2',
+    component.status === 'alert' ? 'text-alert!' : 'text-operating!',
+  ])
+
   return (
-    <div className='m-2 flex flex-1 flex-col overflow-auto border border-zinc-300 p-4'>
-      <span>{component.name}</span>
-      {component.status && <span>{component.status}</span>}
-      {component.sensorType && <span>{component.sensorType}</span>}
-      {component.sensorId && <span>{component.sensorId}</span>}
+    <div className='m-2 flex flex-1 flex-col gap-2 overflow-auto border border-zinc-300 p-4'>
+      <div className='flex gap-2'>
+        <span className='text-xl font-bold uppercase'>{component.name}</span>
+        {component.status && (
+          <>
+            {component.sensorType === 'energy' ? (
+              <EnergySvg className={statusClassName} />
+            ) : (
+              <VibrationSvg className={statusClassName} />
+            )}
+          </>
+        )}
+      </div>
+      <div className='flex flex-col gap-1'>
+        {component.sensorType && (
+          <div className='flex flex-col'>
+            <span className='font-bold'>Equipment type</span>
+            <span className='first-letter:uppercase'>
+              {component.sensorType}
+            </span>
+          </div>
+        )}
+        {component.sensorId && (
+          <div className='flex flex-col'>
+            <span className='font-bold'>Sensor</span>
+            <span>{component.sensorId}</span>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
